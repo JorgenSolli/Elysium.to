61d648f36ad6af5a51de0b5c2dcfcab38b89a030
Jørgen Solli <js@aplia.no>
2019-08-21 11:40:45 +0200

    WIP

--- parts/products.php
+++ parts/products.php
@@ -600,35 +600,44 @@
     <div class="ui stackable equal width grid">
         <div class="column">
             <div class="slide-container">
-                <div class="product-card wizard">
+                <div class="product-card appbox__regular">
                     <div class="product-card__image product-card__image--appbox__regular">
                         <img class="product" alt="Shared Appbox 2" src="images/shared1.svg">
                     </div>
-                    <div class="product-card__level">shared</div>
+                    <div class="product-card__level product-card__level--appbox_regular">shared</div>
                     <div class="product-card__unit-name">APPBOX 1</div>
                     <div class="product-card__unit-description">
-                        The Appbox for all the single men out there! No just kidding you can watch with you wife, too!
+                        The Appbox for all the single people out there! No just kidding you can watch with you spouce, too!
                     </div>
-            
+
                     <div class="product-card__price">
                         <span class="currency">$</span>
-                        <span class="value">15</span>
+                        <span class="value">9.99</span>
                         <span class="period">/MO</span>
                     </div>
-            
+
+                    <div class="product-card__cta">
+                        <button class="product-button">Tell me more...</button>
+                        <button class="product-button">Gimme one!</button>
+                    </div>
+
                     <div class="product-card__unit-stats product-card__unit-stats--appbox_regular clearfix">
                         <div class="one-third">
                             <div class="stat">1</div>
-                            <div class="stat-value">Transcode</div>
+                            <div class="stat-value">Transcodes</div>
                         </div>
             
                         <div class="one-third">
-                            <div class="stat">No</div>
+                            <div class="stat">
+                                <i class="fal fa-times"></i>
+                            </div>
                             <div class="stat-value">4k</div>
                         </div>
             
                         <div class="one-third no-border">
-                            <div class="stat">No</div>
+                            <div class="stat">
+                                <i class="fal fa-times"></i>
+                            </div>
                             <div class="stat-value">Tautulli</div>
                         </div>
                     </div>
@@ -637,11 +646,106 @@
         </div>
         <div class="column">
             <div class="slide-container">
-                <div class="product-card wizard">
+                <div class="product-card appbox_plus">
+                    <div class="product-card__image product-card__image--appbox_plus">
+                        <img class="product" alt="Shared Appbox 2" src="images/dedicated1.svg">
+                    </div>
+                    <div class="product-card__level product-card__level--appbox_plus">shared</div>
+                    <div class="product-card__unit-name">APPBOX+ 1</div>
+                    <div class="product-card__unit-description">
+                        We get it; you like 4k. And possibly Tautulli too! Go ahead and get some use out of that 4k TV you got hanging on the wall!
+                    </div>
+            
+                    <div class="product-card__price">
+                        <span class="currency">$</span>
+                        <span class="value">19.99</span>
+                        <span class="period">/MO</span>
+                    </div>
+
+                    <div class="product-card__cta">
+                        <button class="product-button">Tell me more...</button>
+                        <button class="product-button">Gimme one!</button>
+                    </div>
+
+                    <div class="product-card__unit-stats product-card__unit-stats--appbox_plus clearfix">
+                        <div class="one-third">
+                            <div class="stat">4</div>
+                            <div class="stat-value">Transcodes</div>
+                        </div>
+
+                        <div class="one-third">
+                            <div class="stat">
+                                <i class="fal fa-check"></i>
+                            </div>
+                            <div class="stat-value">4k</div>
+                        </div>
+
+                        <div class="one-third no-border">
+                            <div class="stat">
+                                <i class="fal fa-check"></i>
+                            </div>
+                            <div class="stat-value">Tautulli</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="slide-container">
+                <div class="product-card baremetal">
+                    <div class="product-card__image product-card__image--baremetal">
+                        <img class="product" alt="Shared Appbox 2" src="images/dedicated3.svg">
+                    </div>
+                    <div class="product-card__level product-card__level--baremetal">dedicated</div>
+                    <div class="product-card__unit-name">BAREMETAL 1</div>
+                    <div class="product-card__unit-description">
+                        For all the popular people out there with very demanding friends! Room for all your friends with those big 4k TVs.
+                    </div>
+            
+                    <div class="product-card__price">
+                        <span class="currency">$</span>
+                        <span class="value">60</span>
+                        <span class="period">/MO</span>
+                    </div>
+
+                    <div class="product-card__cta">
+                        <button class="product-button">Tell me more...</button>
+                        <button class="product-button">Gimme one!</button>
+                    </div>
+
+                    <div class="product-card__unit-stats product-card__unit-stats--baremetal clearfix">
+                        <div class="one-third">
+                            <div class="stat">25</div>
+                            <div class="stat-value">Transcodes</div>
+                        </div>
+
+                        <div class="one-third">
+                            <div class="stat">
+                                <i class="fal fa-check"></i>
+                            </div>
+                            <div class="stat-value">4k</div>
+                        </div>
+
+                        <div class="one-third no-border">
+                            <div class="stat">
+                                <i class="fal fa-check"></i>
+                            </div>
+                            <div class="stat-value">Tautulli</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="ui stackable equal width grid">
+        <div class="column">
+            <div class="slide-container">
+                <div class="product-card appbox__regular">
                     <div class="product-card__image product-card__image--appbox__regular">
                         <img class="product" alt="Shared Appbox 2" src="images/shared2.svg">
                     </div>
-                    <div class="product-card__level">shared</div>
+                    <div class="product-card__level product-card__level--appbox_regular">shared</div>
                     <div class="product-card__unit-name">APPBOX 2</div>
                     <div class="product-card__unit-description">
                         Feeling extra kind? Share with a friend or two! Or dont... we're not the boss of you
@@ -656,16 +760,20 @@
                     <div class="product-card__unit-stats product-card__unit-stats--appbox_regular clearfix">
                         <div class="one-third">
                             <div class="stat">3</div>
-                            <div class="stat-value">Transcode</div>
+                            <div class="stat-value">Transcodes</div>
                         </div>
             
                         <div class="one-third">
-                            <div class="stat">No</div>
+                            <div class="stat">
+                                <i class="fal fa-times"></i>
+                            </div>
                             <div class="stat-value">4k</div>
                         </div>
             
                         <div class="one-third no-border">
-                            <div class="stat">No</div>
+                            <div class="stat">
+                                <i class="fal fa-times"></i>
+                            </div>
                             <div class="stat-value">Tautulli</div>
                         </div>
                     </div>
@@ -674,35 +782,80 @@
         </div>
         <div class="column">
             <div class="slide-container">
-                <div class="product-card wizard">
+                <div class="product-card appbox_plus">
                     <div class="product-card__image product-card__image--appbox_plus">
                         <img class="product" alt="Shared Appbox 2" src="images/dedicated1.svg">
                     </div>
-                    <div class="product-card__level">shared</div>
-                    <div class="product-card__unit-name">APPBOX+ 1</div>
+                    <div class="product-card__level product-card__level--appbox_plus">shared</div>
+                    <div class="product-card__unit-name">APPBOX+ 2</div>
                     <div class="product-card__unit-description">
-                        We get it; you like 4k. And possibly Tautulli too! Go ahead and get some use out of that 4k TV you got hanging on the wall!
+                        For all the popular people out there with very demanding friends! Room for all your friends with those big 4k TVs.
                     </div>
             
                     <div class="product-card__price">
                         <span class="currency">$</span>
-                        <span class="value">15</span>
+                        <span class="value">29.99</span>
                         <span class="period">/MO</span>
                     </div>
             
                     <div class="product-card__unit-stats product-card__unit-stats--appbox_plus clearfix">
                         <div class="one-third">
                             <div class="stat">4</div>
-                            <div class="stat-value">Transcode</div>
+                            <div class="stat-value">Transcodes</div>
                         </div>
             
                         <div class="one-third">
-                            <div class="stat">Yes</div>
+                            <div class="stat">
+                                <i class="fal fa-check"></i>
+                            </div>
                             <div class="stat-value">4k</div>
                         </div>
             
                         <div class="one-third no-border">
-                            <div class="stat">Yes</div>
+                            <div class="stat">
+                                <i class="fal fa-check"></i>
+                            </div>
+                            <div class="stat-value">Tautulli</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column">
+            <div class="slide-container">
+                <div class="product-card baremetal">
+                    <div class="product-card__image product-card__image--baremetal">
+                        <img class="product" alt="Shared Appbox 2" src="images/dedicated3.svg">
+                    </div>
+                    <div class="product-card__level product-card__level--baremetal">dedicated</div>
+                    <div class="product-card__unit-name">BAREMETAL 2</div>
+                    <div class="product-card__unit-description">
+                        For all the popular people out there with very demanding friends! Room for all your friends with those big 4k TVs.
+                    </div>
+            
+                    <div class="product-card__price">
+                        <span class="currency">$</span>
+                        <span class="value">75</span>
+                        <span class="period">/MO</span>
+                    </div>
+            
+                    <div class="product-card__unit-stats product-card__unit-stats--baremetal clearfix">
+                        <div class="one-third">
+                            <div class="stat">25</div>
+                            <div class="stat-value">Transcodes</div>
+                        </div>
+            
+                        <div class="one-third">
+                            <div class="stat">
+                                <i class="fal fa-check"></i>
+                            </div>
+                            <div class="stat-value">4k</div>
+                        </div>
+            
+                        <div class="one-third no-border">
+                            <div class="stat">
+                                <i class="fal fa-check"></i>
+                            </div>
                             <div class="stat-value">Tautulli</div>
                         </div>
                     </div>
